6467d252e13b5fee0f30fce1037931ce3526b54a
Rick Obi <sanunes.ricardo@gmail.com>
2020-11-03 18:50:59 -0500

    UTIL | Added resume and other as valid interfaces

--- src/app/shared/card-edit/edit/async.service.ts
+++ src/app/shared/card-edit/edit/async.service.ts
@@ -1,36 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { RepositoryService } from './../../repository.service';
-import { LinkInterface, ContactInterface } from './../../../_interface/personal.model';
+import { PersonalInterface } from './../../../_interface/personal.model';
 import { ExperienceInterface } from './../../../_interface/experience.model';
 import { EducationInterface } from './../../../_interface/education.model';
 import { SkillInterface } from './../../../_interface/skill.model';
+import { OtherInterface } from './../../../_interface/other.model';
+import { ResumeClass } from './../../../_interface/resume.model';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsyncService<T extends
-  ContactInterface|LinkInterface|ExperienceInterface|EducationInterface|SkillInterface
+  PersonalInterface|ExperienceInterface|EducationInterface|SkillInterface|OtherInterface|ResumeClass
   >{
   endPoint: string;
-  data: T[];
+  slug: string;
+  data: T;
+  datas: T[];
 
-  constructor(private repoService: RepositoryService){
-    console.log(repoService);
+  constructor(private repoService: RepositoryService){}
+
+  async getDetails(slug: string): Promise<T> {
+    const prom = await this.repoService.getData(`${this.endPoint}${slug}`)
+    .toPromise()
+    .then(res => {
+      this.data = res as T;
+    });
+    return this.data;
   }
 
   async getNotInCurrent(current: T[]): Promise<T[]>{
     const prom = await this.repoService.getData(this.endPoint)
       .toPromise()
       .then(res => {
-        this.data = (res as T[]).filter(function(value, index){
-          return !current.some((d) => {
-            d.id ? d.id === value.id : d.slug === value.slug
-          });
+        this.datas = (res as T[]).filter(function(value, index){
+          return !current.some((d) => d.id === value.id );
         });
       });
-      return this.data;
+      return this.datas;
   }
 
   async create(data: T){
